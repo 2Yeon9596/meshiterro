8d2d24087d002a131db3818fb470119a1afe0749
출구없정연이의입구 <jeongyeononly1101@chulgueobsjeong-yeon-iuiibgunoMacBook-Air.local>
2025-03-23 20:36:58 +0900

    users show,edit,update作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,9 @@ class User < ApplicationRecord
 
   def get_profile_image
     unless profile_image.attached?
-      file_path = Rails.root.join("app/assets/images/sample-author1.jpg")
-      profile_image.attach(io: File.open(file_path), filename: "default-image.jpg", content_type: "image/jpeg")
-    end 
-    profile_image.variant(resize_to_limit: [100, 100]).processed
+      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image
   end  
 end
