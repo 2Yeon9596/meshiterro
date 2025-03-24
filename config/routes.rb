1d078d499cb3d53733aea9170a3f6872e6518932
출구없정연이의입구 <jeongyeononly1101@chulgueobsjeong-yeon-iuiibgunoMacBook-Air.local>
2025-03-24 23:43:24 +0900

    レイアウト調整

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,11 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get '/about' => "homes#about", as: :about
 
+  devise_for :users
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resource :favorite, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end  
   resources :users, only: [:show, :edit, :update]
-  devise_for :users
-
-  devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
   
 end
